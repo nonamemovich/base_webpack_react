9653611b385be52e9179719480f9f485a0ee1888
noname <nonamemovich@gmail.com>
2020-02-24 17:40:06 +0500

    remove class components Recall

--- src/components/Recall/index.jsx
+++ src/components/Recall/index.jsx
@@ -1,7 +1,7 @@
 import React, { PureComponent, useState } from 'react';
 import styles from './styles.css';
 
-const funcComponent = function() {
+export default Recall = function() {
 
   const [mouseOverIndex, setMouseOver] = useState(null);
 
@@ -49,77 +49,3 @@ const funcComponent = function() {
     </div>
   );
 }
-
-export class Recall extends PureComponent {
-  constructor(props) {
-    super(props)
-    this.state = {
-      mouseOverIndex: null,
-      rating: '',
-    };
-    this.setMouseOver = this.setMouseOver.bind(this);
-    this.handleStarClick = this.handleStarClick.bind(this);
-  }
-
-  setMouseOver(mouseOverIndex) {
-    console.warn('asdasd');
-    this.setState((state)=>({
-      state: state.rating,
-      mouseOverIndex,
-    }));
-  }
-
-  handleStarClick(rating) {
-    this.setState((state)=>({
-      mouseOverIndex: state.mouseOverIndex,
-      rating,
-    }));
-  }
-
-  render() {
-    const { mouseOverIndex, rating } = this.state;
-    return (
-      <div className={styles["container"]}>
-        <h1 className={styles["title"]}>Рейтинг меню: {rating}</h1>
-        <h1 className={styles["title"]}>mouseOverIndex: {mouseOverIndex}</h1>
-        <div>
-          <div className={styles["stars"]}>
-            {[5, 4, 3, 2, 1].map(index => {
-              let filled = index <= mouseOverIndex ? styles['star-filled'] : '';
-
-              return (
-              <div
-                  key={index}
-                  onMouseLeave={()=> {this.setMouseOver(null)}}
-                  onMouseEnter={()=> {this.setMouseOver(index)}}
-                  className={styles['star'] + ' ' +filled}
-                  onClick={()=> {this.handleStarClick(index)}}
-                />
-              )}
-            )}
-          </div>
-        </div>
-        <form>
-          <input
-            type="text"
-            id="username"
-            required
-            placeholder="Enter Name"
-            onChange={ (event)=>{
-              console.warn(event.target.setCustomValidity);
-
-              if (event.target.value=='' && event.target.setCustomValidity) {
-                event.target.setCustomValidity('custom validate');
-              } else {
-                event.target.setCustomValidity('');
-              }
-            }}
-          />
-          <input type='submit'/>
-        </form>
-      </div>
-    );
-  }
-}
-
-export default Recall;
